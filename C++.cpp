85011e11237c78f08783dddec177c5b8c5f39105
Reginaldo Gama Junior <reginaldojrgama@gmail.com>
2025-06-03 15:06:28 -0300

    Adicionando o 2.Add Two Numbers

--- C++.cpp
+++ C++.cpp
@@ -13,3 +13,44 @@ public:
     }
 };
 //////////////////////////////////////////////////////////////////////
+// 2.Add Two Numbers ////////////////////////////////////////////////
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* head = new ListNode(0);
+        ListNode* current = head;
+        int carry = 0;
+        while(l1 != NULL || l2 != NULL){
+            int x,y,soma;
+            if(l1 != NULL){
+                x = l1 -> val;
+            }else{
+                    x = 0; 
+            }
+            if(l2 != NULL){
+                y = l2 -> val;
+            }else{
+                    y = 0;    
+            }
+            soma = carry + x + y;
+
+            carry = int(soma/10);
+            current -> next = new ListNode(soma % 10);
+            current = current -> next;
+
+            if(l1 != NULL){
+                l1 = l1 -> next;
+            }
+            if(l2 != NULL){
+                l2 = l2 -> next;
+            }
+                        
+        }
+        if (carry > 0){
+            current -> next = new ListNode(carry);
+        }
+    return head -> next;
+        
+    }
+};
+//////////////////////////////////////////////////////////////////////////////////
--- Python.py
+++ Python.py
@@ -7,3 +7,26 @@ class Solution:
                 if nums[i] + nums[j] == target:
                     return [i,j]
 #######################################################################
+## 2.Add Two Numbers ##################################################
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        head = ListNode(0)
+        current = head
+        carry = 0
+
+        while l1 is not None or l2 is not None:
+            x = l1.val if l1 else 0
+            y = l2.val if l2 else 0
+            sum = carry + x + y
+
+            carry = sum // 10
+            current.next = ListNode(sum % 10)
+            current = current.next
+
+            if l1: l1 = l1.next
+            if l2: l2 = l2.next
+
+        if carry > 0:
+            current.next = ListNode(carry)
+        return head.next
+#########################################################################
