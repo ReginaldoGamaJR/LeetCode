6831ff9b821794caf821de7a13e358b3e5f4edb8
Reginaldo Gama Junior <reginaldojrgama@gmail.com>
2025-06-06 11:11:07 -0300

    Adicionando questão 20. Valid Parentheses

--- C++.cpp
+++ C++.cpp
@@ -130,3 +130,25 @@ public:
     }
 };
 /////////////////////////////////////////////////////////////////////////////
+// 20. Valid Parentheses ////////////////////////////////////////////////////
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+        for (char c : s){
+            if(c == '(' || c == '{' || c == '['){
+                stk.push(c);
+            } else {
+                if(stk.empty()) return false;
+                if ((c == ')' && stk.top() != '(') ||
+                    (c == '}' && stk.top() != '{') ||
+                    (c == ']' && stk.top() != '[')) {
+                    return false;
+                }
+                stk.pop();
+            }
+        }
+        return stk.empty();
+    }
+};
+/////////////////////////////////////////////////////////////////////////////
